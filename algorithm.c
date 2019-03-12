a182a83cb95d5c59f8092fd6ec2a5fcee732cf16
DenisSemerych <denis_semerych@icloud.com>
2019-03-12 12:34:17 +0200

    one_more_test

--- algorithm.c
+++ algorithm.c
@@ -6,65 +6,26 @@
 /*   By: dsemeryc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 20:05:34 by dsemeryc          #+#    #+#             */
-/*   Updated: 2019/03/12 12:31:12 by dsemeryc         ###   ########.fr       */
+/*   Updated: 2019/03/12 12:34:06 by dsemeryc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/lem_in.h"
 
-void		find_and_close(t_list *paths)
-{
-	t_list *crawler;
-	t_room *max;
-	int max_count;
-	t_room *room;
-	int count;
-
-	max = NULL;
-	max_count = 0;
-	while (paths)
-	{
-		crawler = paths->content;
-		while (crawler)
-		{
-			room = crawler->content;
-			if ((count = list_count(room->links)) > max_count && !room->is_start && !room->is_end)
-			{
-				max_count = count;
-				max = room;
-			}
-			crawler = crawler->next;
-		}
-		paths = paths->next;
-	}
-	if (max)
-		max->is_closed = 1;
-}
-
 t_list		*algorithm(t_list **rooms, int num_of_ants)
 {
 	t_list	*path;
 	t_list	*tmp_paths;
-	t_list	*solution_with_closed;
-	t_list	*solution_without_closed;
+	t_list	*solution;
 
-	solution_without_closed = NULL;
+	solution = NULL;
 	while ((path = bfs(rooms)))
 	{
 		tmp_paths = path;
-		solution_without_closed = add_to_the_end_of_list(solution_without_closed, tmp_paths);
+		solution = add_to_the_end_of_list(solution_without_closed, tmp_paths);
 		clear_rooms(rooms, solution_without_closed);
 	}
-	solution_with_closed = NULL;
-	clear_rooms(rooms, NULL);
-	find_and_close(solution_without_closed);
-	while ((path = bfs(rooms)))
-	{
-		tmp_paths = path;
-		solution_with_closed = add_to_the_end_of_list(solution_with_closed, tmp_paths);
-		clear_rooms(rooms, solution_with_closed);
-	}
-	return (solution_with_closed);
+	return (solution);
 }
 
 t_list		*give_start_room(t_list **rooms)
